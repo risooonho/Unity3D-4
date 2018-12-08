5d1d54e2e479fc7443fb1404a2ccafc18cae27a9
Andrew Morse <contactandrewmorse@gmail.com>
2018-12-08 07:04:31 -0500

    Create InventoryController.cs
    Currently working on further functionality of in-game inventory canvas controller.

---
+++ Inventory/InventoryController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour {
+
+    // Initialize to avoid null reference
+    private bool isOn = false;
+
+    public GameObject inventoryCanvas;
+
+	// Update is called once per frame
+	void Update () {
+
+        // Toggle ON/OFF
+        if (Input.GetKeyDown(KeyCode.I))
+            isOn = !isOn;
+
+        // Check ON/OFF
+        if (isOn)
+            inventoryCanvas.SetActive(true);
+        else
+            inventoryCanvas.SetActive(false);
+    }
+}
