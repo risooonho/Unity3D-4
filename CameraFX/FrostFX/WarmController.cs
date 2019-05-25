d630c4e790183a377cebd6fd8d6edf460c399b6b
Andrew Morse <contactandrewmorse@gmail.com>
2019-05-25 07:38:11 -0400

    Add files via upload
    Inverse controller of frost for area triggers.

---
+++ CameraFX/FrostFX/WarmController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarmController : MonoBehaviour
+{
+    private GameObject player;
+    private GameObject frostZone;
+    private FrostEffect playerScript;
+    private const float coef = 0.1f;
+    private bool detect;
+
+    // TODO: Collision detection implementation
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.player = GameObject.Find("FPSCamera");
+        this.playerScript = player.GetComponent<FrostEffect>();
+        this.frostZone = GameObject.Find("WarmZone");
+        this.playerScript.FrostAmount = 0f;
+    }
+
+    private void Update()
+    {
+        if (detect) 
+        {
+
+            // TODO: Adjust later for testing
+            // Lower bound for frost limit 
+            if (playerScript.FrostAmount < 0f)
+                return;
+
+            this.playerScript.FrostAmount -= coef * Time.deltaTime;    
+            Debug.Log("Current frost value is " + playerScript.FrostAmount);  
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "Camera&Controller")
+            detect = true;
+        else
+            detect = false;
+    }
+    // Testing
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Camera&Controller")
+            detect = false;
+        else
+            detect = true;
+
+    }
+}
