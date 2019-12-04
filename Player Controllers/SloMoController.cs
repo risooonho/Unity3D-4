a0eec3765faba50f2fffc2c5aa23dcb4a060198c
Andrew Morse <contactandrewmorse@gmail.com>
2019-12-04 01:31:18 -0500

    Update SloMoController.cs

--- Player Controllers/SloMoController.cs
+++ Player Controllers/SloMoController.cs
@@ -2,14 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Note: Attach to main player
+
 public class SloMoController : MonoBehaviour
 {
+    public float timer = 10.0f;
+    public bool activated;
 
-    [Range(0, 1)]
-    public float TimeScale = 1.0f;
     // Update is called once per frame
     void Update()
     {
-        Time.timeScale = TimeScale;   
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Time.timeScale = 0.65f;
+            activated = true;
+        }
+
+        if (activated)
+        {
+            if (timer <= 0)
+            {
+                Time.timeScale = 1.0f;
+                activated = false;
+                timer = 10.0f;
+            }
+
+            timer -= Time.deltaTime;
+        } 
     }
 }
