2a2363e0942f5f014b4b78fb32e00f523ff20e25
Andrew Morse <contactandrewmorse@gmail.com>
2019-12-05 00:24:47 -0500

    Update SloMoController.cs
    Updated w/ sound clips and timer.

--- Player Controllers/SloMoController.cs
+++ Player Controllers/SloMoController.cs
@@ -3,30 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // Note: Attach to main player
-
 public class SloMoController : MonoBehaviour
 {
-    public float timer = 10.0f;
-    public bool activated;
+    public static float timer = 10.0f;
+    public bool activationFlag;
+    public AudioClip startSound;
+    public AudioClip endSound;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        // Gate it
+        if(!activationFlag)
         {
-            Time.timeScale = 0.65f;
-            activated = true;
+            if(Input.GetKeyDown(KeyCode.F))
+            {
+                AudioSource start = GetComponent<AudioSource>();
+                start.clip = startSound;
+                start.Play();
+
+                Time.timeScale = 0.5f;
+                activationFlag = true;   
+            }
         }
 
-        if (activated)
+        if(activationFlag)
         {
             if (timer <= 0)
             {
+                // Intialize audio clip when timer runs out
+                AudioSource end = GetComponent<AudioSource>();
+                end.clip = endSound;
+                end.Play();
+
+                // Reset all values when timer runs out
                 Time.timeScale = 1.0f;
-                activated = false;
+                activationFlag = false;
                 timer = 10.0f;
+                Debug.Log(Time.timeScale);
+
             }
 
+            // Timer countdown starts
             timer -= Time.deltaTime;
         } 
     }
