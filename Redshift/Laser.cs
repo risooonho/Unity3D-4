787215aedba91fc42cef7c95fc5b15804ae3421f
Andrew Morse <contactandrewmorse@gmail.com>
2020-01-16 14:24:50 -0500

    Update Laser.cs
    Laser property script updated.

--- Redshift/Laser.cs
+++ Redshift/Laser.cs
@@ -1,1 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 8.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Speed of laser
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        if (transform.position.y > 8.0f) {
+            Destroy(this.gameObject);
+        }
+          
+    }
+}
