7164db166137055d23774c105e0381ba5ea0d55d
Andrew Morse <contactandrewmorse@gmail.com>
2020-01-17 15:34:40 -0500

    Create Enemy.cs
    Basic enemy component script.

---
+++ Redshift/Enemy.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Enemy speed
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        // Randomize enemy spawn 
+        if (transform.position.y < -5.0f)
+        {
+            float randomX = Random.Range(-8f, 8f);
+            transform.position = new Vector3(randomX, 7, 0);   
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerScript player = other.transform.GetComponent<PlayerScript>();
+
+            if (player != null)
+            {
+                player.Damage();
+            }
+         
+            Destroy(this.gameObject);
+        }
+
+        if (other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
