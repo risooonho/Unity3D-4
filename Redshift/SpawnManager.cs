4b45a06df741bcab205ac1656720b63b95ad2750
Andrew Morse <contactandrewmorse@gmail.com>
2020-01-21 22:04:41 -0500

    Update SpawnManager.cs

--- Redshift/SpawnManager.cs
+++ Redshift/SpawnManager.cs
@@ -10,27 +10,44 @@ public class SpawnManager : MonoBehaviour
     [SerializeField]
     private GameObject _enemyContainer;
 
+    [SerializeField]
+    private GameObject _tripleShotPowerupPrefab;
+
+    private bool _stopSpawning = false;
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnRoutine());
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        StartCoroutine(SpawnPowerupRoutine());
     }
 
     IEnumerator SpawnRoutine ()
     {
 
-        while (true)
+        while (_stopSpawning == false)
         {
             Vector3 spawnPos = new Vector3(Random.Range(-8f, 8f), 7, 0);
             GameObject newEnemy = Instantiate(_enemyPrefab, spawnPos, Quaternion.identity);
             newEnemy.transform.parent = _enemyContainer.transform;
             yield return new WaitForSeconds(5.0f);
         }
+    }
+
+    IEnumerator SpawnPowerupRoutine()
+    {
+        while (_stopSpawning == false)
+        {
+            Vector3 postToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
+            Instantiate(_tripleShotPowerupPrefab, postToSpawn, Quaternion.identity);
+            yield return new WaitForSeconds(Random.Range(3, 8));
+        }
+
+    }
+
+    // Helper for cross script
+    public void OnPlayerDeath()
+    {
+        _stopSpawning = true;
     }
 }
