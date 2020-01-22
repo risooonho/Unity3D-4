290d7eb2b7d28df42d89944e79c2bb1463bce297
Andrew Morse <contactandrewmorse@gmail.com>
2020-01-21 22:06:01 -0500

    Create Powerup.cs

---
+++ Redshift/Powerup.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime); // Movement speed
+
+        // Destroy once out of bounds
+        if (transform.position.y < -5.0f)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerScript player = other.transform.GetComponent<PlayerScript>();
+
+            // NULL Check
+            if (player != null)
+            {
+                player.TripleShotActive();
+            }
+
+            Destroy(this.gameObject);
+        }
+        
+    }
+}
